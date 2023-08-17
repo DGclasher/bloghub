d4c03633672ed5e690ef0f874c6709984f7f0a15
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-08-17 19:11:23 +0530

    dev

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path
 from . import views
+from .views import *
+from django.urls import path
 from django.contrib.auth import views as auth_views
 
 
 app_name = "users"
 
 urlpatterns = [
-    path('register/', views.register, name='users-register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html') , name="users-login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html') , name="users-logout"),
     path('profile/', views.profile, name="users-profile"),
+    path('my_posts/', views.my_posts, name='users-my_posts'),
+    path('register/', views.register, name='users-register'),
+    path('login/', CustomLogin.as_view() , name="users-login"),
     path('update_profile/', views.update_profile, name='users-update_profile'),
-    path('my_posts/', views.my_posts, name='users-my_posts')
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html') , name="users-logout"),
 ] 
 
